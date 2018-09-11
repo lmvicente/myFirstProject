1d7c9e9c9b9825e51db5ec3fc38598bbce2d80aa
unknown <leah.m.vicente@gmail.com>
2018-09-11 16:41:18 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var myFirstProgram = /** @class */ (function () {
+    function myFirstProgram() {
+        var argArray = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            argArray[_i] = arguments[_i];
+        }
+        this.printArg(argArray);
+    }
+    myFirstProgram.prototype.printArg = function (argArray) {
+        console.log(argArray);
+    };
+    myFirstProgram.prototype.runFirst = function (callback) {
+        console.log("This method runs first ...");
+    };
+    myFirstProgram.prototype.runLast = function () {
+        console.log("This methods runs last ...");
+    };
+    return myFirstProgram;
+}());
+var newInstance = new myFirstProgram("a", "b", "c");
---
+++ main.ts
@@ -0,0 +1,16 @@
+class myFirstProgram {
+    constructor(...argArray: Array<any>) {
+        this.printArg(argArray);
+    }
+    printArg(argArray) {
+        console.log(argArray);
+    }
+    runFirst(callback: Function) {
+        console.log("This method runs first ...");
+    }
+
+    runLast() {
+        console.log("This methods runs last ...");
+    }
+}
+const newInstance = new myFirstProgram("a", "b", "c");
